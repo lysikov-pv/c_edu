b615fd539335e43512601eee1143b4b949f0c234
Petr Lysikov <p.v.lysikov@gmail.com>
2021-12-14 13:28:13 +0500

    Задачи 63-65. Семинар 11.

---
+++ les_11/ex_64/Program.cs
@@ -0,0 +1,58 @@
+﻿// 64. Показать треугольник Паскаля. Сделать вывод в виде равнобедренного треугольника.
+
+int[,] GetPascalTriangle(int size) // Заполняет массив как треугольник Паскаля
+{
+    int[,] array = new int[size, size + 1];
+    array[0, 1] = 1;
+    for (int i = 1; i < array.GetLength(0); i++)
+        for (int j = 1; j < array.GetLength(1); j++)
+            array[i, j] = array[i - 1, j - 1] + array[i - 1, j ];
+    return array;
+}
+
+void PrintSpaces(int size) // Выводит заданное количество пробелов
+{
+    for (int i = 0; i < size; i++) Console.Write(" ");
+}
+
+void PrintTriangleArray(int[,] arr) // Выводит на экран элементы двумерного массива
+{
+    int spaces = arr.GetLength(0) * 2; // Магические числа для табуляции
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        PrintSpaces(spaces);
+        spaces -= 2;  // Магические числа для табуляции
+        for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                if (arr[i, j] != 0) Console.Write(String.Format("{0,4:0}", arr[i, j]));
+                else Console.Write("     ");
+            }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+
+{   // Тест 1
+    Console.WriteLine("Тест 1");
+    int[,] triangle = GetPascalTriangle(size:1);
+    PrintTriangleArray(triangle);
+}
+
+{   // Тест 2
+    Console.WriteLine("Тест 2");
+    int[,] triangle = GetPascalTriangle(size:2);
+    PrintTriangleArray(triangle);
+}
+
+{   // Тест 3
+    Console.WriteLine("Тест 3");
+    int[,] triangle = GetPascalTriangle(size:6);
+    PrintTriangleArray(triangle);
+}
+
+{   // Тест 4
+    Console.WriteLine("Тест 4");
+    int[,] triangle = GetPascalTriangle(size:13);
+    PrintTriangleArray(triangle);
+}
---
+++ les_11/ex_65/Program.cs
@@ -0,0 +1,150 @@
+﻿// 65. Спирально заполнить двумерный массив:
+//   1  2  3  4
+//  12 13 14  5
+//  11 16 15  6
+//  10  9  8  7 
+
+int[,] GetSpiralFill(int rows, int columns) // Заполняет массив спирально
+{
+    int[,] array = new int[rows, columns];
+    int i = 0;
+    int j = 0;
+    int count = 1;
+    string direction = "right";
+    int changeDirection = 0;
+    while (changeDirection < 2)
+    {
+
+        if (changeDirection == 0)
+        {
+            array[i, j] = count;
+            count++;
+        }
+        switch (direction)
+        {
+            case "right":
+            {
+                if (j + 1 < columns ) // Справа не край массива
+                    if (array[i, j + 1] == 0) // Справа пустая ячейка
+                    {
+                        j++;
+                        changeDirection = 0;
+                    }
+                    else // Справа ячейка заполнена
+                    {
+                        direction = "down";
+                        changeDirection++;
+                    }
+                else // Справа край массива
+                {
+                    direction = "down";
+                    changeDirection++;
+                }
+                break;
+            }
+            case "down":
+            {
+                if (i + 1 < rows ) // Снизу не край массива
+                    if (array[i + 1, j] == 0) // Снизу пустая ячейка
+                    {
+                        i++;
+                        changeDirection = 0;
+                    }
+                    else // Сниху ячейка заполнена
+                    {
+                        direction = "left";
+                        changeDirection++;
+                    }
+                else // Снизу край массива
+                {
+                    direction = "left";
+                    changeDirection++;
+                }
+                break;
+            }
+            case "left":
+            {
+                if (j - 1 >= 0 ) // Слева не край массива
+                    if (array[i, j - 1] == 0) // Слева пустая ячейка
+                    {
+                        j--;
+                        changeDirection = 0;
+                    }
+                    else // Слева ячейка заполнена
+                    {
+                        direction = "top";
+                        changeDirection++;
+                    }
+                else // Слева край массива
+                {
+                    direction = "top";
+                    changeDirection++;
+                }
+                break;
+            }
+            case "top":
+            {
+                if (i - 1 >= 0 ) // Сверху не край массива
+                    if (array[i - 1, j] == 0) // Сверху пустая ячейка
+                    {
+                        i--;
+                        changeDirection = 0;
+                    }
+                    else // Сверху ячейка заполнена
+                    {
+                        direction = "right";
+                        changeDirection++;
+                    }
+                else // Сверху край массива
+                {
+                    direction = "right";
+                    changeDirection++;
+                }
+                break;
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] arr, string preStr = "", string postStr = "\n") // Выводит на экран элементы двумерного массива
+{
+    Console.Write(preStr);
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write(String.Format("{0,5:0}", arr[i, j]));
+        Console.WriteLine();
+    }
+    Console.Write(postStr);
+}
+
+{   // Тест 1
+    Console.WriteLine("Тест 1");
+    int[,] array = GetSpiralFill(rows: 1, columns: 6);
+    PrintArray(array, preStr: "", postStr: "");
+}
+
+{   // Тест 2
+    Console.WriteLine("Тест 2");
+    int[,] array = GetSpiralFill(rows: 7, columns: 1);
+    PrintArray(array, preStr: "", postStr: "");
+}
+
+{   // Тест 3
+    Console.WriteLine("Тест 3");
+    int[,] array = GetSpiralFill(rows: 2, columns: 5);
+    PrintArray(array, preStr: "", postStr: "");
+}
+
+{   // Тест 4
+    Console.WriteLine("Тест 4");
+    int[,] array = GetSpiralFill(rows: 3, columns: 8);
+    PrintArray(array, preStr: "", postStr: "");
+}
+
+{   // Тест 5
+    Console.WriteLine("Тест 5");
+    int[,] array = GetSpiralFill(rows: 9, columns: 6);
+    PrintArray(array, preStr: "", postStr: "");
+}
